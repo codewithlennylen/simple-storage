64b99698fd250a59aa5f69f4534345f6283f934b
codewithlennylen <codewithlennylen254@gmail.com>
2024-10-26 17:09:04 +0300

    Structs and Arrays

--- simple_storag.sol
+++ simple_storag.sol
@@ -4,15 +4,34 @@ pragma solidity ^0.8.19;
 
 contract SimpleStorage {
 
-    uint256 favoriteNumber;
+    uint256 myFavoriteNumber;
+
+
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+
+    // Person public me = Person({favoriteNumber:24, name:"Lenny"});
+    // Person public jane = Person({favoriteNumber:2, name:"Jane"});
+
+    // dynamic array
+    Person[] public listOfPeople;
+
+    // static array
+    // Person[] public listOfPeople;
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public{
+        listOfPeople.push(Person({favoriteNumber: _favoriteNumber, name:_name}));
+    }
 
     function Store(uint256 _favoriteNumber) public {
 
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function retrive() public view returns(uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
     }
 
 }
